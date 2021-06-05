4795bcd925da809c0e37e448c35855c16b45dd57
mojodojo101 <malte.luhmann@yahoo.de>
2021-06-06 00:02:28 +0200

    fixed image refs

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -29,13 +29,13 @@ const resource = [
   /* --- Favicons --- */
   {% assign favicon_path = "/assets/img/favicons" | relative_url %}
 
-  '{{ favicon_path }}/android-chrome-192x192.png',
-  '{{ favicon_path }}/android-chrome-512x512.png',
-  '{{ favicon_path }}/apple-touch-icon.png',
-  '{{ favicon_path }}/favicon-16x16.png',
-  '{{ favicon_path }}/favicon-32x32.png',
+  '{{ favicon_path }}/android-chrome-192x192.jpg',
+  '{{ favicon_path }}/android-chrome-512x512.jpg',
+  '{{ favicon_path }}/apple-touch-icon.jpg',
+  '{{ favicon_path }}/favicon-16x16.jpg',
+  '{{ favicon_path }}/favicon-32x32.jpg',
   '{{ favicon_path }}/favicon.ico',
-  '{{ favicon_path }}/mstile-150x150.png',
+  '{{ favicon_path }}/mstile-150x150.jpg',
   '{{ favicon_path }}/site.webmanifest',
   '{{ favicon_path }}/browserconfig.xml'
 
